93dce348eb89fb761d6f6a3213c0c03630d4d237
Karthik Shenoy <kashenoy@microsoft.com>
2025-03-28 00:48:20 +0530

    fixed container reporting

--- Pragmatism/Server/internal/handlers/http/taskhandlerutils.go
+++ Pragmatism/Server/internal/handlers/http/taskhandlerutils.go
@@ -6,26 +6,26 @@ import (
 )
 
 const SUCCESS_INDICATOR_UNICODE_CHAR = '\u2713'
-const FAILURE_INDICATOR_UNICODE_CHAR = '\u2715'
+const FAILURE_INDICATOR_UNICODE_CHAR = '\u00D7'
 
 func GetTaskResultFromTestResult(testResult string) []*api.TestResult {
 	taskResult := make([]*api.TestResult, 0)
 	testResultLines := strings.Split(testResult, "\n")
 	for _, line := range testResultLines {
-		for index, c := range line {
-			testResult := &api.TestResult{}
-			if c == FAILURE_INDICATOR_UNICODE_CHAR {
-				testResult.IsSuccessful = false
-				testResult.TestName = string([]rune(line)[index+2:])
-				taskResult = append(taskResult, testResult)
-				break
-			} else if c == SUCCESS_INDICATOR_UNICODE_CHAR {
-				testResult.IsSuccessful = true
-				testResult.TestName = string([]rune(line)[index+2:])
-				taskResult = append(taskResult, testResult)
-				break
-			}
-
+		sentences := strings.Split(line, "> ")
+		if len(sentences) < 2 {
+			continue
+		}
+		firstChar := []rune(strings.TrimSpace(sentences[0]))[0]
+		testResult := &api.TestResult{}
+		if firstChar == FAILURE_INDICATOR_UNICODE_CHAR {
+			testResult.IsSuccessful = false
+			testResult.TestName = strings.TrimSpace(sentences[1])
+			taskResult = append(taskResult, testResult)
+		} else if firstChar == SUCCESS_INDICATOR_UNICODE_CHAR {
+			testResult.IsSuccessful = true
+			testResult.TestName = strings.TrimSpace(sentences[1])
+			taskResult = append(taskResult, testResult)
 		}
 	}
 	return taskResult
