02800c1178e8664b632a539f126dd8eacc708628
Karthik Shenoy <kashenoy@microsoft.com>
2025-08-17 20:23:11 +0530

    moved otel to same compose

--- Pragmatism/Server/internal/services/telemetryservice/temelemtryServiceConstants.go
+++ Pragmatism/Server/internal/services/telemetryservice/temelemtryServiceConstants.go
@@ -6,7 +6,7 @@ func getOtelEndPointUrl() string {
 	if cmdflags.IsDevMode() {
 		return "localhost:4318"
 	}
-	return "localhost:4138"
+	return "otel-collector:4318"
 }
 
 const TELEMETRY_SERVICE_NAME = "PragmatismWebApp.TelemetryService"
